73724b698c83b32e403a5b588f02e9ed89a5fadb
ScottRudiger <scottrudiger@gmail.com>
2017-07-17 15:26:03 -0700

    (test) Test User table

--- test/databaseTest.js
+++ test/databaseTest.js
@@ -1,37 +1,22 @@
-var db = require('../server/db/index.js');
 const expect = require('chai').expect;
+const db = require('../server/db/index.js');
 
-function makeUser(callback) {
-
-return db.User.create({
-    username: 'Scott'
-  })
-  .then(function() {
-    callback();
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
-}
-
-function getUser() {
-  return db.User.findOne({where:{username: 'Scott'}})
-  .then(function(user) {
-    console.log(user);
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
-}
 
 describe('first db test', () => {
   it('should retrieve a user', (done) => {
-    makeUser(function() {
-      getUser.then(function() {
-        expect(user.username).to.equal('Scott');
-        done();
-      });
+    db.models.User.create({username: 'Scott'})
+    .then(() => {
+      return db.models.User.findOne({ where: {username: 'Scott'} });
+    })
+    .then((user) => {
+      expect(user.username).to.equal('Scott');
+      db.close();
+      done();
+    })
+    .catch((err) => {
+      console.error(err);
+      db.close();
+      done();
     });
   });
 });
-
