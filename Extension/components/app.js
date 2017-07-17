ae41a41ae111c5210a2741c140cbdb51ec65d1c1
or4nga <patrick.tang1086@gmail.com>
2017-07-17 17:53:28 -0500

    Built extension dropdown view

--- Extension/components/app.js
+++ Extension/components/app.js
@@ -1,6 +1,40 @@
 angular.module('app', [])
 
-  .controller('AppCtrl', function() {
+  .controller('AppCtrl', function($http) {
+
+    var that = this;
+
+    this.tabUrl = '';
+    this.loggedIn = false;
+    this.rating = '80%' // on init - get page rating from DB
+    this.rated = true;
+
+    this.get_current_url = function(callback) {
+      chrome.tabs.query({ active: true }, function(tabs) {
+        that.tabUrl = tabs[0].url;
+        callback(that.tabUrl)
+        // return tabUrl;
+      });
+    }
+
+    this.getdata = function(url) {
+      console.log('url', url)
+      // $http.get('/urls' + url, function(res, req) {
+      //   // get back data
+      //   // set this.rating = data.rating
+      // })
+    }
+
+    // get_current_url(function(url) {
+    //   this.tabUrl = url;
+    // });
+
+
+    this.handleSubmitComment = function(comment) {
+      //post comment to DB
+      // $http.post()
+      this.comment = '';
+    }
 
   })
 
