830cfcf226a8f720e564e25d9418570a1e7d2493
ScottRudiger <scottrudiger@gmail.com>
2017-07-17 15:54:52 -0700

    (test) Add test for creating and retrieving a user

--- test/databaseTest.js
+++ test/databaseTest.js
@@ -4,12 +4,12 @@ const db = require('../server/db/index.js');
 
 describe('first db test', () => {
   it('should retrieve a user', (done) => {
-    db.models.User.create({username: 'Scott'})
+    db.models.User.create({username: 'John'})
     .then(() => {
-      return db.models.User.findOne({ where: {username: 'Scott'} });
+      return db.models.User.findOne({ where: {username: 'John'} });
     })
     .then((user) => {
-      expect(user.username).to.equal('Scott');
+      expect(user.username).to.equal('John');
       db.close();
       done();
     })
