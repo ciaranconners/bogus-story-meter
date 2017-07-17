5577b89ec124c19c0292e704dbc6210e2861009e
ScottRudiger <scottrudiger@gmail.com>
2017-07-17 01:10:21 -0700

    (deploy) change port # for deployment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var app = require('./routes.js');
 
-var port = process.env.port || 3000;
+var port = process.env.port || 1337;
 
 module.exports = app.listen(port, function() {
 	console.log('listening on port ' + port);
