5b0942c10ee2d5957077faf1cca707701efaf500
Ciaran Martin Conners <ciaranconners@gmail.com>
2017-07-18 19:34:31 -0400

    Implementing Foreign Keys Via Sequelize Schema Definition

--- test/setUpTest.js
+++ test/setUpTest.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 
-describe('sanity check', function() {
-  it('should return true', function() {
+describe('sanity check', () => {
+  it('should return true', () => {
     expect(true).to.equal(true);
   });
 });
