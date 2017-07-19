9645f8a412e6c2352593e5b7fe974007ea1c7cbb
ScottRudiger <scottrudiger@gmail.com>
2017-07-19 15:25:13 -0700

    (feat) Add endpoint & functionality for url voting (WIP)

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,12 +1,39 @@
-var handler = {};
+const db = require('./db/index.js');
+
+const handler = {};
 
 handler.serveIndexGetRequest = function(req, res) {
   res.status(200).send('hello world');
 };
 
 handler.test = function(req, res) {
-  console.log('Hitting test endpoint')
-  res.status(200).send('hey world')
+  console.log('Hitting test endpoint');
+  res.status(200).send('hey world');
+};
+
+handler.getUrlVotes = () => {
+
+};
+
+handler.postUrlVotes = (req, res) => {
+  let url = req.body.url;
+  let type = req.body.type;
+  let username = req.body.username;
+  let typeCount = type === 'upvote' ? 'upvoteCount' : type === 'downvote' ? 'downvoteCount' : 'neutralCount';
+  let resObj = {};
+  db.Url.findCreateFind({where: {url: url}})
+    .then(url => {
+      return url.increment(typeCount)
+        .then(() => resObj.url = url);
+    })
+    .then(() => db.User.findCreateFind({where: {username: username}}))
+    .then(user => {
+      return user.increment(typeCount)
+        .then(() => resObj.user = user);
+    })
+    .then(() => {
+      res.status(201).send('Vote recorded');
+    });
 };
 
 module.exports = handler;
--- server/routes.js
+++ server/routes.js
@@ -1,24 +1,27 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var multer = require('multer');
 var handler = require('./request-handler.js');
 var app = express();
 var path = require('path');
 var cors = require('cors');
 
-// app.use(cors());
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', handler.serveIndexGetRequest);
+// app.get('/', handler.serveIndexGetRequest);
+
+app.get('/urlvote', handler.getUrlVotes);
+app.post('/urlvote', handler.postUrlVotes);
 
 app.get('/test', handler.test);
 
