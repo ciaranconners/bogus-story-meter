4d027892eda501ed8c9df46dfd99ca7cff4903ba
ScottRudiger <scottrudiger@gmail.com>
2017-07-20 03:39:43 -0700

    (fix/refactor) Fix/refactor dynamic icon
    (fix) Fix GET request for updating url rating when address in tab changes
    (refactor) Save serverUri in a variable so it's easier to deploy

--- extension/background.js
+++ extension/background.js
@@ -1,43 +1,52 @@
-
-// listener for page load?  tab switch?
-
-// get rating from database
-// var rating = 10;
-
 const updateIcon = (rating) => {
-  if (rating === null) {
-    chrome.browserAction.setIcon({path: '../images/BSMIcon.png'});
-    chrome.browserAction.setBadgeBackgroundColor({color: 'transparent'});
-    chrome.browserAction.setBadgeText({text: ''});
-  } else if (rating >= 60) {
-    chrome.browserAction.setIcon({path: '../images/BSMIconGreen.png'});
-    chrome.browserAction.setBadgeBackgroundColor({color: 'green'});
-    chrome.browserAction.setBadgeText({text: `${rating}%`});
-  } else if (rating < 60) {
-    chrome.browserAction.setIcon({path: '../images/BSMIconRed.png'});
-    chrome.browserAction.setBadgeBackgroundColor({color: 'red'});
-    chrome.browserAction.setBadgeText({text: `${rating}%`});
-  }
+  const CBA = chrome.browserAction;
+
+  const updateIconTo = {
+    notRatedOrMiddle: () => {
+      CBA.setIcon({path: '../images/BSMIcon.png'});
+      CBA.setBadgeBackgroundColor({color: [0, 0, 0, 0]});
+      CBA.setBadgeText({text: ''});
+    },
+    truthy: () => {
+      CBA.setIcon({path: '../images/BSMIconGreen.png'});
+      CBA.setBadgeBackgroundColor({color: 'green'});
+      CBA.setBadgeText({text: `${rating}%`});
+    },
+    falsy: () => {
+      CBA.setIcon({path: '../images/BSMIconRed.png'});
+      CBA.setBadgeBackgroundColor({color: 'red'});
+      CBA.setBadgeText({text: `${rating}%`});
+    }
+  };
+  /*eslint-disable indent*/
+  rating === null ? updateIconTo.notRatedOrMiddle()
+  : rating >= 60 ? updateIconTo.truthy()
+  : rating <= 60 ? updateIconTo.falsy()
+  : updateIconTo.notRatedOrMiddle();
+  /*eslint-enable indent*/
 };
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete') {
-    if (tab.url !== 'about:blank' && tab.url !== 'chrome://newtab/') {
-      var url = tab.url;
-      console.log('updatedUrl', url);
+let lastUrl;
+// get rating for url when address on current tab changes
+chrome.tabs.onUpdated.addListener(function(tabId, tab) {
+  // Note to Ciaran: removed "if (changeInfo.status === 'complete')" because it was making the icon update after a long delay on sites with a lot of ads. the below if statement allows it to load faster while repeating the GET request at most twice.
+  // Feel free to delete this and ^ that after you read it
+  let url = tab.url;
+  if (url !== lastUrl) {
+    if (url !== 'about:blank' && url !== 'chrome://newtab/') {
       $.ajax({
-        type: 'POST',
-        url: 'http://localhost:8080/urlrating',
+        type: 'GET',
+        url: `${window.serverUri}/urlrating`,
+        params: {},
         data: {
           currentUrl: url
         },
         error: function(err) {
-          console.log('FAIL');
+          console.log(`Failed to get rating for ${url}`);
           console.error(err);
         },
         success: function(data) {
-          console.log('success');
-          console.log('DATA', data);
+          lastUrl = url;
           updateIcon(JSON.parse(data));
         }
       });
@@ -45,26 +54,23 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   }
 });
 
+// get rating for url after switching tabs
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-  console.log($.ajax);
   chrome.tabs.get(activeInfo.tabId, function(tab) {
-    if (tab.url !== 'about:blank' && tab.url !== 'chrome://newtab/') {
-      var url = tab.url;
-      console.log('activatedUrl', url);
+    let url = tab.url;
+    if (url !== 'about:blank' && url !== 'chrome://newtab/') {
       $.ajax({
         type: 'GET',
-        url: 'http://localhost:8080/urlrating',
+        url: `${window.serverUri}/urlrating`,
         params: {},
         data: {
           currentUrl: url
         },
         error: function(err) {
-          console.log('FAIL');
+          console.log(`Failed to get rating for ${url}`);
           console.error(err);
         },
         success: function(data) {
-          console.log('success');
-          console.log('DATA', data);
           updateIcon(JSON.parse(data));
         }
       });
