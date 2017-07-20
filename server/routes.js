356c147c54c367a45ddd147ffec7eae30000d9fc
Ciaran Martin Conners <ciaranconners@gmail.com>
2017-07-20 14:44:52 -0400

    Cleanup to Routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,25 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var multer = require('multer');
 var path = require('path');
-var cors = require('cors');
 var handler = require('./request-handler.js');
 
-
 var app = express();
-
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '../public')));
-
-// app.get('/', handler.serveIndexGetRequest);
 
 app.get('/urlvote/:urlId', handler.getUrlVotes);
 
