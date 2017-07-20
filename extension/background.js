c59d15c5ac24705c8df68f12a9a40596eb6de533
Ciaran Martin Conners <ciaranconners@gmail.com>
2017-07-19 21:46:57 -0400

    Updates Current URL Not Just on Page Load But Also on Tab Selection

--- extension/background.js
+++ extension/background.js
@@ -18,18 +18,39 @@ if(rating === null) {
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status === 'complete') {
-    console.log(tab.url);
-  var url = tab.url;
-  $.ajax({
-    type: "POST",
-    url: 'http://localhost:8080/url',
-    data: {
-      currentUrl: url
-    },
-    success: function(data) {
-      console.log('success');
-    },
-    dataType: 'application/json'
-  });
+    // console.log(tab.url);
+    if (tab.url !== 'about:blank' && tab.url !== 'chrome://newtab/') {
+      var url = tab.url;
+      $.ajax({
+        type: "POST",
+        url: 'http://localhost:8080/url',
+        data: {
+          currentUrl: url
+        },
+        success: function(data) {
+          console.log('success');
+        },
+        dataType: 'application/json'
+      });
+    }
   }
+});
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  chrome.tabs.get(activeInfo.tabId, function(tab) {
+    if (tab.url !== 'about:blank' && tab.url !== 'chrome://newtab/') {
+      var url = tab.url;
+      $.ajax({
+        type: "POST",
+        url: 'http://localhost:8080/url',
+        data: {
+          currentUrl: url
+        },
+        success: function(data) {
+          console.log('success');
+        },
+        dataType: 'application/json'
+      });
+    }
+  });
 });
