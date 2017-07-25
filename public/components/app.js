4e6a55bcf9e1f6b37c0fa5c63b78e241a4769157
Ciaran Martin Conners <ciaranconners@gmail.com>
2017-07-24 21:26:03 -0400

    After Revert

--- public/components/app.js
+++ public/components/app.js
@@ -1,44 +1,60 @@
 angular.module('app')
 
-.controller('AppCtrl', function($scope, requests) {
-	this.profileInfo = {};
-	this.profileInfo.name = '';
-	this.profileInfo.url = '';
-	this.profileInfo.email = '';
-	this.profileInfo.id = '';
-	this.signedIn = false;
+.controller('AppCtrl', ['$rootScope', function($scope, requests, GAuth, $rootScope) {
+	// this.profileInfo = {};
+	// this.profileInfo.name = '';
+	// this.profileInfo.url = '';
+	// this.profileInfo.email = '';
+	// this.profileInfo.id = '';
+	// this.signedIn = false;
 
-	this.onSignIn = function(googleUser) {
-	  var profile = googleUser.getBasicProfile();
-	  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-	  console.log('Name: ' + profile.getName());
-	  console.log('Image URL: ' + profile.getImageUrl());
-	  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+this.handleLogin = function() {
+  console.log('inside doSignup');
+  GAuth.login().then(function(user) {
+    console.log(user.name);
+  }, function() {
+    console.log('login failed');
+  });
+};
 
-  	this.profileInfo.name = profile.getName();
-  	this.profileInfo.url = profile.getImageUrl();
-  	this.profileInfo.email = profile.getEmail();
-  	this.profileInfo.id = profile.getId();
+this.getUserInfo = function() {
+  console.log('RS: ', $rootScope);
+  // console.log($rootScope.gdata.getUser().name);
+};
 
-  	// this.signedIn = true;
-    if (this.profileInfo.url.length > 1) {
-      this.signedIn = true;
-      console.log(this.signedIn);
-    }
-	  $scope.$apply();
-	}.bind(this);
+	// this.onSignIn = function(googleUser) {
+	//   var profile = googleUser.getBasicProfile();
+	//   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	//   console.log('Name: ' + profile.getName());
+	//   console.log('Image URL: ' + profile.getImageUrl());
+	//   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
-	window.onSignIn = this.onSignIn;
+ //  	this.profileInfo.name = profile.getName();
+ //  	this.profileInfo.url = profile.getImageUrl();
+ //  	this.profileInfo.email = profile.getEmail();
+ //  	this.profileInfo.id = profile.getId();
 
-  this.signOut = function() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-    this.signedIn = false;
-  };
-  console.log(this.signedIn);
-})
+ //  	// this.signedIn = true;
+ //    if (this.profileInfo.url.length > 1) {
+ //      this.signedIn = true;
+ //      console.log(this.signedIn);
+ //    }
+	//   $scope.$apply();
+	// }.bind(this);
+
+// 	window.onSignIn = this.onSignIn;
+
+//   this.signOut = function() {
+//     var auth2 = gapi.auth2.getAuthInstance();
+//     auth2.signOut().then(function () {
+//       console.log('User signed out.');
+//     });
+//     this.signedIn = false;
+//   };
+//   console.log(this.signedIn);
+this.getUserInfo();
+this.handleLogin();
+}])
 .component('app', {
   templateUrl: './templates/app.html',
   controller: 'AppCtrl'
--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngRoute'])
+angular.module('app', ['ngRoute', 'angular-google-gapi'])
 
 .config(['$locationProvider', '$routeProvider',
   function config($locationProvider, $routeProvider) {
@@ -18,4 +18,39 @@ angular.module('app', ['ngRoute'])
       //   redirectTo: '/'
       // });
   }
+])
+.run(['GAuth', 'GApi', 'GData', '$rootScope',
+    function(GAuth, GApi, GData, $rootScope) {
+
+        $rootScope.gdata = GData;
+
+        var CLIENT = '266363285075-j69o8kv6pq8agofle7sfvqap59ceqsfu';
+        var BASE = 'http:localhost:8080';
+
+        // GApi.load('myApiName','v1',BASE);
+        // GApi.load('calendar','v3'); // for google api (https://developers.google.com/apis-explorer/)
+
+        GAuth.setClient(CLIENT);
+        // default scope is only https://www.googleapis.com/auth/userinfo.email
+        GAuth.setScope('https://www.googleapis.com/auth/userinfo.email');
+
+        // load the auth api so that it doesn't have to be loaded asynchronously
+        // when the user clicks the 'login' button.
+        // That would lead to popup blockers blocking the auth window
+        GAuth.load();
+
+        // or just call checkAuth, which in turn does load the oauth api.
+        // if you do that, GAuth.load(); is unnecessary
+        // GAuth.checkAuth().then(
+        //     function (user) {
+        //         console.log(user.name + ' is logged in');
+        //         $state.go('webapp.home'); // an example of action if it's possible to
+        //                           // authenticate user at startup of the application
+        //     },
+        //     function() {
+        //     $state.go('login'); // an example of action if it's impossible to
+        //                 // authenticate user at startup of the application
+        //     }
+        // );
+    }
 ]);
