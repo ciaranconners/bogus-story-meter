58ad953895bd7c58f578ecc9802c3dee8174c6c5
johnmrox <johnmrox@gmail.com>
2017-07-24 22:28:27 -0400

    service implemented for website

--- public/services/requests.js
+++ public/services/requests.js
@@ -1,4 +1,41 @@
 angular.module('app')
 
 .service('requests', function($http, $window) {
+	this.get = (endpoint, data = {}, params = {}, errMsg, callback) => {
+		$http({
+			method: 'GET',
+			url: endpoint,
+			params: params,
+			data: data
+		})
+		.then(response => {
+			callback(response.data);
+		})
+		.catch(error => console.error(errMsg, error));
+	};
+
+	this.post = (endpoint, data, errMsg, callback) => {
+		$http({
+			method: 'POST',
+			url: endpoint,
+			data: data
+		})
+		.then(response => {
+			callback(response.data);
+		})
+		.catch(error => console.error(errMsg, error));
+	};
+
+	this.put = (endpoint, data = {}, params = {}, errMsg, callback) => {
+		$http({
+			method: 'PUT',
+			url: endpoint,
+			params: params,
+			data: data
+		})
+		.then(response => {
+			callback(response.data);
+		})
+		.catch(error => console.error(errMsg, error));
+	};
 });
