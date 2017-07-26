5127b25da2d9e492c4818c0e143a594b861c022f
ScottRudiger <scottrudiger@gmail.com>
2017-07-26 02:38:10 -0700

    (feat) Create Comment and Reply components for stat page rendering (WIP)

---
+++ public/components/reply.js
@@ -0,0 +1,9 @@
+angular.module('app') /*eslint-disable indent*/
+.component('reply', {
+  templateUrl: '../templates/reply.html',
+
+  bindings: {
+    reply: '<'
+  }
+});
+
--- public/components/stats.js
+++ public/components/stats.js
@@ -2,16 +2,38 @@ angular.module('app')/*eslint-disable indent*/
 
 
 .controller('StatCtrl', function(request, $location) {
+
   this.url;
+  this.rating;
+  this.comments = [
+    {
+      username: 'User One',
+      text: 'comment1',
+      replies: [{username: 'User Two', text: 'comment1a'}]
+    },
+    {username: 'User Three', text: 'comment2', replies: []},
+    {username: 'User Four', text: 'comment3', replies: []}
+  ];
+
   this.getUrlStats = function() {
     let path = $location.url().split('/');
     let urlId = path[path.length - 1];
     let errMsg = 'couldn\'t get URL stats';
     let params = {urlId: urlId};
     request.get('/urlstats', null, params, errMsg, (response) => {
+      // this.url = response.url;
       this.url = response;
+      // this.rating = response.rating;
+      // this.comments = response.comments;
       console.log('this.url', this.url);
+      console.log('this.url', this.rating);
+      console.log('this.url', this.comments);
     });
   };
   this.getUrlStats();
+
+  this.handleReply = function(commentId) {
+
+  };
+
 });
