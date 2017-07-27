452bb7f7e5c9f8acda964941e86ea3bee6ddff27
ScottRudiger <scottrudiger@gmail.com>
2017-07-27 12:30:37 -0700

    (feat) add preliminary functionality to stats page for presentation purposes

--- public/components/stats.js
+++ public/components/stats.js
@@ -7,6 +7,8 @@ angular.module('app') /*eslint-disable indent*/
   this.rating;
   this.comments;
   this.urlId;
+  this.rated = true; // set up request to see if user voted
+  this.replying = false;
 
   const getUrlId = () => {
     let path = $location.url().split('/');
@@ -19,10 +21,12 @@ angular.module('app') /*eslint-disable indent*/
     request.get('/urlstats', null, params, errMsg, res => {
     this.url = res.url;
     this.rating = res.rating;
+    console.log(this.rating);
     });
   };
 
   const getUrlComments = () => {
+    console.log('IN GETURLCOMMENTS');
     let errMsg = 'couldn\'t get URL comments';
     let params = {urlId: getUrlId()};
     request.get('/urlcomments', null, params, errMsg, res => {
@@ -35,8 +39,23 @@ angular.module('app') /*eslint-disable indent*/
     getUrlComments();
   });
 
-  this.handleReply = function(commentId) {
+  this.commentText = '';
+  this.replyText = '';
 
+  this.setReplying = () => {
+    this.replying = !this.replying;
+  };
+
+  this.postComment = (text, commentId = null) => {
+    console.log('in post comment');
+    this.commentText = '';
+    this.replyText = '';
+    let errMsg = 'failed to post comment';
+    let data = {urlId: getUrlId(), comment: text, commentId: commentId};
+    request.post('/urlcomment', data, errMsg, res => {
+      console.log('posted the comment');
+      // getUrlComments(); // get comments after posting or refactor to socket.io
+    });
   };
 
 });
