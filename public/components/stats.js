4e93844ec7af52a5b5908cf81cef5dd03d7bd561
ScottRudiger <scottrudiger@gmail.com>
2017-07-29 19:41:43 -0700

    (fix) render new comments/replies on stats view upon posting

--- public/components/stats.js
+++ public/components/stats.js
@@ -10,7 +10,8 @@ angular.module('app') /*eslint-disable indent*/
   this.urlId;
   this.rated;
   this.userVote = null;
-  this.replying = false;
+  this.commentText = '';
+  this.replyText = '';
 
   const getUrlId = () => {
     let path = $location.url().split('/');
@@ -21,16 +22,15 @@ angular.module('app') /*eslint-disable indent*/
     let errMsg = 'couldn\'t get URL stats';
     let params = {urlId: this.urlId};
     request.get('/urlstats', null, params, errMsg, res => {
-    this.url = res.url;
-    this.username = res.username;
-    this.rating = res.rating;
-    this.rating || this.rating === 0 ? this.rated = true : this.rated = false;
-    this.userVote = res.vote;
+      this.url = res.url;
+      this.username = res.username;
+      this.rating = res.rating;
+      this.rating || this.rating === 0 ? this.rated = true : this.rated = false;
+      this.userVote = res.vote;
     });
   };
 
-  this.getUrlComments = () => {
-    console.log('IN GETURLCOMMENTS');
+  const getUrlComments = () => {
     let errMsg = 'couldn\'t get URL comments';
     let params = {urlId: this.urlId};
     request.get('/urlcomments', null, params, errMsg, res => {
@@ -41,64 +41,50 @@ angular.module('app') /*eslint-disable indent*/
   $scope.$on('$routeChangeSuccess', () => {
     getUrlId();
     getUrlStats();
-    this.getUrlComments();
+    getUrlComments();
   });
 
-  this.commentText = '';
-  this.replyText = '';
-
-  this.toggleReplying = () => {
-    console.log('in replying');
-    this.replying = !this.replying;
-  };
-
+  // posts both comments and replies
   this.postComment = (text, commentId = null) => {
-    console.log('in post comment');
-    this.commentText = '';
-    this.replyText = '';
     let errMsg = 'failed to post comment';
     let data = {urlId: this.urlId, comment: text, commentId: commentId};
+    this.commentText = '';
+    this.replyText = '';
     request.post('/urlcomment', data, errMsg, res => {
-      console.log('posted the comment');
+      getUrlComments();
     });
   };
 
-  this.handleVote = (vote) => {
+  this.handleVote = vote => {
     if (this.url === null) {
       return;
     }
-    var data = {
+    let data = {
       urlId: this.urlId,
       url: this.url,
       username: this.username,
       type: vote
     };
     let errMsg = 'Could not submit vote: ';
-
-    console.log('inside handlevote - data ', data);
-
     // if user hasnt voted before, new vote:
     if (this.userVote === null) {
-      request.post('/urlvote', data, errMsg, (res) => {
+      request.post('/urlvote', data, errMsg, res => {
         this.urlId = res;
-        request.get(`/urlvote/${data.urlId}`, null, null, errMsg, (res) => {
+        request.get(`/urlvote/${data.urlId}`, null, null, errMsg, res => {
           this.rating = res;
           this.rated = true;
           this.userVote = vote;
-          // chrome.runtime.sendMessage({'rating': that.rating, 'userVote': that.userVote, 'urlId': that.urlId});
         });
       });
     } else if (this.userVote !== vote) { // if user is changing vote
-      request.put('/urlvote', data, errMsg, (res) => {
+      request.put('/urlvote', data, errMsg, res => {
         this.urlId = res;
-        request.get(`/urlvote/${data.urlId}`, null, null, errMsg, (res) => {
+        request.get(`/urlvote/${data.urlId}`, null, null, errMsg, res => {
           this.rating = res;
           this.rated = true;
           this.userVote = vote;
-          // chrome.runtime.sendMessage({'rating': this.rating, 'userVote': this.userVote, 'urlId': that.urlId});
         });
       });
-
     } else if (this.userVote === vote) {
       return; // don't let user send same rating twice for same URL
     }
