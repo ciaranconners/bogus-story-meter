dc51ad96729761d06ed9612749602cb5749a542f
Ciaran Martin Conners <ciaranconners@gmail.com>
2017-08-01 17:21:31 -0400

    some formatting changes, experimenting with prettier

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,5 +1,5 @@
-const db = require('./db/index.js');
-const utils = require('./utils.js');
+const db = require("./db/index.js");
+const utils = require("./utils.js");
 
 const handler = {};
 
@@ -9,59 +9,70 @@ handler.getUrlData = (req, res) => {
   let username = req.query.currentUser;
   let profilepicture = req.query.currentProfilePicture;
   let fullname = req.query.currentName;
-  db.User.findCreateFind( {where: {'username': username, 'profilepicture': profilepicture, 'fullname': fullname}} )
-  .spread((userEntry) => {
-    if (userEntry !== null) {
-    return db.Url.findOne( {where: {url: url}} )
-    .then((urlEntry) => {
-      if (urlEntry === null) {
-        res.json( {
-          'rating': null,
-          'urlId': null,
-          'userId': userEntry.id,
-          'userVote': null
-        });
-      } else {
-        return db.UrlVote.findOne( {where: {'userId': userEntry.id, 'urlId': urlEntry.id}} )
-        .then((voteEntry) => {
-          rating = utils.calculateRating(urlEntry.upvoteCount, urlEntry.downvoteCount);
-          if (voteEntry) {
-            res.json( {
-              'rating': rating,
-              'urlId': urlEntry.id,
-              'userId': userEntry.id,
-              'userVote': voteEntry.type
+  db.User
+    .findCreateFind({
+      where: {
+        username: username,
+        profilepicture: profilepicture,
+        fullname: fullname
+      }
+    })
+    .spread(userEntry => {
+      if (userEntry !== null) {
+        return db.Url.findOne({ where: { url: url } }).then(urlEntry => {
+          if (urlEntry === null) {
+            res.json({
+              rating: null,
+              urlId: null,
+              userId: userEntry.id,
+              userVote: null
             });
           } else {
-            res.json( {
-              'rating': rating,
-              'urlId': urlEntry.id,
-              'userId': userEntry.id,
-              'userVote': null
-            });
+            return db.UrlVote
+              .findOne({ where: { userId: userEntry.id, urlId: urlEntry.id } })
+              .then(voteEntry => {
+                rating = utils.calculateRating(
+                  urlEntry.upvoteCount,
+                  urlEntry.downvoteCount
+                );
+                if (voteEntry) {
+                  res.json({
+                    rating: rating,
+                    urlId: urlEntry.id,
+                    userId: userEntry.id,
+                    userVote: voteEntry.type
+                  });
+                } else {
+                  res.json({
+                    rating: rating,
+                    urlId: urlEntry.id,
+                    userId: userEntry.id,
+                    userVote: null
+                  });
+                }
+              });
           }
         });
       }
     });
-  }
-  });
 };
 
 // the following function will generate a new stat page url or retrieve one if it exists in the DB
 handler.generateRetrieveStatsPageUrl = (req, res) => {
   let currentUrl = req.query.currentUrl;
-  db.Url.findCreateFind({
+  db.Url
+    .findCreateFind({
       where: {
         url: currentUrl
       }
     })
     .spread(url => {
-      let stpUrl = 'http://localhost:8080' + '/stats/redirect/' + url.id.toString();
-      console.log('new stat page URL created, transmitting: ', stpUrl);
+      let stpUrl =
+        "http://localhost:8080" + "/stats/redirect/" + url.id.toString();
+      console.log("new stat page URL created, transmitting: ", stpUrl);
       res.status(200).json(stpUrl);
-
     })
-    .catch((err) => {
+    .catch(err => {
       console.error(err);
       res.sendStatus(500);
     });
@@ -70,70 +81,78 @@ handler.generateRetrieveStatsPageUrl = (req, res) => {
 // this function should check if there's a session first, otherwise you get type errors when the user is set to null
 handler.getUrlStats = (req, res) => {
   let urlId = req.query.urlId;
-  let urlData = {username: req.session.username};
-  db.Url.findOne({where: {id: urlId}})
-  .then(urlEntry => {
-    urlData.url = urlEntry.url;
-    urlData.rating = utils.calculateRating(urlEntry.upvoteCount, urlEntry.downvoteCount);
-  })
-  .then(() => {
-    return db.User.findOne({where: {username: urlData.username}});
-  })
-  .then(user => {
-    if (user !== null) {
-    return db.UrlVote.findOne({where: {userId: user.id, urlId: urlId}});
-  }
-  })
-  .then(vote => {
-    vote ? urlData.vote = vote.type : urlData.vote = null;
-    res.send(urlData);
-  })
-  .catch(err => {
-    console.error(err);
-    res.sendStatus(500);
-  });
+  let urlData = { username: req.session.username };
+  db.Url
+    .findOne({ where: { id: urlId } })
+    .then(urlEntry => {
+      urlData.url = urlEntry.url;
+      urlData.rating = utils.calculateRating(
+        urlEntry.upvoteCount,
+        urlEntry.downvoteCount
+      );
+    })
+    .then(() => {
+      return db.User.findOne({ where: { username: urlData.username } });
+    })
+    .then(user => {
+      if (user !== null) {
+        return db.UrlVote.findOne({ where: { userId: user.id, urlId: urlId } });
+      }
+    })
+    .then(vote => {
+      vote ? (urlData.vote = vote.type) : (urlData.vote = null);
+      res.send(urlData);
+    })
+    .catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 };
 
 handler.getAllActivity = (req, res) => {
-  db.Url.findAll({'limit': 15})
-  .then(activity => {
+  db.Url.findAll({ limit: 15 }).then(activity => {
     res.status(200).json(activity);
   });
 };
 
 handler.getUserActivity = (req, res) => {
   let username = req.query.username;
-  db.User.findOne( {'where': {'username': username}} )
-  .then((userEntry) => {
-    return db.UrlVote.findAll( {'where': {'userId': userEntry.id}} )
-    .then((userVotes) => {
-      return db.Comment.findAll( {'where': {'userId': userEntry.id}} )
-      .then((userComments) => {
+  db.User.findOne({ where: { username: username } }).then(userEntry => {
+    return db.UrlVote
+      .findAll({ where: { userId: userEntry.id } })
+      .then(userVotes => {
+        return db.Comment
+          .findAll({ where: { userId: userEntry.id } })
+          .then(userComments => {
+            let userVotesPromises = userVotes.map(row => {
+              return db.Url
+                .findOne({ where: { id: row.urlId } })
+                .then(urlEntry => {
+                  row.dataValues.url = urlEntry.url;
+                  row.dataValues.title = urlEntry.title;
+                  return row;
+                });
+            });
 
-        let userVotesPromises = userVotes.map((row) => {
-          return db.Url.findOne( {'where': {'id': row.urlId}} )
-          .then((urlEntry) => {
-            row.dataValues.url = urlEntry.url;
-            row.dataValues.title = urlEntry.title;
-            return row;
+            let userCommentsPromises = userComments.map(row => {
+              return db.Url
+                .findOne({ where: { id: row.urlId } })
+                .then(urlEntry => {
+                  row.dataValues.url = urlEntry.url;
+                  row.dataValues.title = urlEntry.title;
+                  return row;
+                });
+            });
+            Promise.all(userVotesPromises).then(userVotesNew => {
+              Promise.all(userCommentsPromises).then(userCommentsNew => {
+                res.json({
+                  userComments: userCommentsNew,
+                  userVotes: userVotesNew
+                });
+              });
+            });
           });
-        });
-
-        let userCommentsPromises = userComments.map((row) => {
-          return db.Url.findOne( {'where': {'id': row.urlId}} )
-          .then((urlEntry) => {
-            row.dataValues.url = urlEntry.url;
-            row.dataValues.title = urlEntry.title;
-            return row;
-          });
-        });
-        Promise.all(userVotesPromises).then((userVotesNew) => {
-          Promise.all(userCommentsPromises).then((userCommentsNew) => {
-            res.json({'userComments': userCommentsNew, 'userVotes': userVotesNew});
-          });
-        });
       });
-    });
   });
 };
 
--- server/requestHandlers/vote.js
+++ server/requestHandlers/vote.js
@@ -1,14 +1,12 @@
-// inside function to post votes, save watson data to elasticsearch
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const db = require('../db/index.js');
-const utils = require('../utils.js');
+const db = require("../db/index.js");
+const utils = require("../utils.js");
 
-const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
-const elasticsearch = require('elasticsearch');
-const watsonConfig = require('./watson-config.js');
+const NaturalLanguageUnderstandingV1 = require("watson-developer-cloud/natural-language-understanding/v1.js");
+const elasticsearch = require("elasticsearch");
+const watsonConfig = require("./watson-config.js");
 
 const username = watsonConfig.username;
 const password = watsonConfig.password;
@@ -16,41 +14,47 @@ const password = watsonConfig.password;
 const nlu = new NaturalLanguageUnderstandingV1({
   username: username,
   password: password,
-  version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27,
+  version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
 });
 
 const client = new elasticsearch.Client({
-  host: 'localhost:9200',
-  log: 'error'
+  host: "localhost:9200",
+  log: "error"
 });
 
 const getFromWatson = (url, callback) => {
-  nlu.analyze({
-  'url': url,
-  'features': {
-    'categories' : {},
-    'metadata': {}
-  },
-  'return_analyzed_text': true
-}, (err, response) => {
-    callback(err, response);
- });
+  nlu.analyze(
+    {
+      url: url,
+      features: {
+        categories: {},
+        metadata: {}
+      },
+      return_analyzed_text: true
+    },
+    (err, response) => {
+      callback(err, response);
+    }
+  );
 };
 
 // body => {title: 'title', text: 'text', categories: 'categories'}
 
 const saveToElasticsearch = (index, type, id, body, callback) => {
-  client.index({
-    index: index,
-    type: type,
-    id: id,
-    body: body
-  }, (error, response) => {
+  client.index(
+    {
+      index: index,
+      type: type,
+      id: id,
+      body: body
+    },
+    (error, response) => {
       callback(err, response);
-  });
+    }
+  );
 };
 
-router.post('/', (req, res, next) => {
+router.post("/", (req, res, next) => {
   let url = req.body.url;
   let urlId = req.body.urlId;
   let type = req.body.type;
@@ -58,151 +62,176 @@ router.post('/', (req, res, next) => {
   let typeCount = `${type}Count`;
 
   if (urlId !== null) {
-    db.Url.findOne({where: {id: urlId}})
-    .then(url => {
-      return db.User.findCreateFind({where: {username: username}})
-      .spread(user => {
-        db.UrlVote.create({type: type, userId: user.id, urlId: url.id})
-        .then(() => {
-          db.User.increment(typeCount, {where: {id: user.id}});
-          db.Url.increment(typeCount, {where: {id: url.id}});
-          res.status(201).json(url.id);
-        })
-        .catch(err => {
-          res.sendStatus(400);
-        });
+    db.Url
+      .findOne({ where: { id: urlId } })
+      .then(url => {
+        return db.User
+          .findCreateFind({ where: { username: username } })
+          .spread(user => {
+            db.UrlVote
+              .create({ type: type, userId: user.id, urlId: url.id })
+              .then(() => {
+                db.User.increment(typeCount, { where: { id: user.id } });
+                db.Url.increment(typeCount, { where: { id: url.id } });
+                res.status(201).json(url.id);
+              })
+              .catch(err => {
+                res.sendStatus(400);
+              });
+          })
+          .catch(err => {
+            res.sendStatus(400);
+          });
       })
       .catch(err => {
         res.sendStatus(400);
       });
-    })
-    .catch(err => {
-      res.sendStatus(400);
-    });
   } else if (urlId === null) {
-    db.Url.findCreateFind({where: {'url': url}})
-    .spread(url => {
+    db.Url.findCreateFind({ where: { url: url } }).spread(url => {
       getFromWatson(url.url, (err, data) => {
         if (err) {
           console.error(err);
-          return db.Url.increment(typeCount, {where: {id: url.id}})
-                .then(() => {
-                  return db.User.findCreateFind({where: {username: username}})
-                    .spread(user => {
-                      db.UrlVote.create({type: type, userId: user.id, urlId: url.id})
-                        .then(() => {
-                          db.User.increment(typeCount, {where: {id: user.id}});
-                          res.status(201).json(url.id);
-                          url.update({categoryId: category.id, title: title})
-                            .catch((err) => {
-                              console.error(err);
-                            });
-                        }).catch(err => {
-                          res.sendStatus(400);
+          return db.Url
+            .increment(typeCount, { where: { id: url.id } })
+            .then(() => {
+              return db.User
+                .findCreateFind({ where: { username: username } })
+                .spread(user => {
+                  db.UrlVote
+                    .create({ type: type, userId: user.id, urlId: url.id })
+                    .then(() => {
+                      db.User.increment(typeCount, { where: { id: user.id } });
+                      res.status(201).json(url.id);
+                      url
+                        .update({ categoryId: category.id, title: title })
+                        .catch(err => {
+                          console.error(err);
                         });
-                    }).catch(err => {
+                    })
+                    .catch(err => {
                       res.sendStatus(400);
                     });
-                }).catch(err => {
-                res.sendStatus(400);
-              });
+                })
+                .catch(err => {
+                  res.sendStatus(400);
+                });
+            })
+            .catch(err => {
+              res.sendStatus(400);
+            });
         } else {
           let title = data.metadata.title;
           categories = [];
           for (let x of data.categories) {
             categories.push(x.label.slice(1));
           }
-          let category = categories.join(' ');
-          db.Category.findCreateFind({where: {name: category}})
-            .spread((category) => {
-              return db.Url.increment(typeCount, {where: {id: url.id}})
+          let category = categories.join(" ");
+          db.Category
+            .findCreateFind({ where: { name: category } })
+            .spread(category => {
+              return db.Url
+                .increment(typeCount, { where: { id: url.id } })
                 .then(() => {
-                  return db.User.findCreateFind({where: {username: username}})
+                  return db.User
+                    .findCreateFind({ where: { username: username } })
                     .spread(user => {
-                      db.UrlVote.create({type: type, userId: user.id, urlId: url.id})
+                      db.UrlVote
+                        .create({ type: type, userId: user.id, urlId: url.id })
                         .then(() => {
-                          db.User.increment(typeCount, {where: {id: user.id}});
+                          db.User.increment(typeCount, {
+                            where: { id: user.id }
+                          });
                           res.status(201).json(url.id);
-                          url.update({categoryId: category.id, title: title})
-                            .catch((err) => {
+                          url
+                            .update({ categoryId: category.id, title: title })
+                            .catch(err => {
                               console.error(err);
                             });
-                        }).catch(err => {
+                        })
+                        .catch(err => {
                           res.sendStatus(400);
                         });
-                    }).catch(err => {
+                    })
+                    .catch(err => {
                       res.sendStatus(400);
                     });
-                }).catch(err => {
-                res.sendStatus(400);
-              });
+                })
+                .catch(err => {
+                  res.sendStatus(400);
+                });
             });
         }
       });
     });
-   }
+  }
 });
 
-
-
-router.put('/', (req, res, next) => {
+router.put("/", (req, res, next) => {
   let url = req.body.url;
   let urlId = req.body.urlId;
   let type = req.body.type;
   let username = req.body.username || req.session.username;
   let typeCount = `${type}Count`;
-  db.Url.findOne({where: {id: urlId}})
-  .then(urlEntry => {
-    return db.User.findOne({where: {username: username}})
-    .then(userEntry => {
-      return db.UrlVote.findOne({where: {userId: userEntry.id, urlId: urlEntry.id}})
-      .then(voteEntry => {
-        let oldTypeCount = `${voteEntry.type}Count`;
-        let oldType = voteEntry.type;
-        userEntry.decrement(oldTypeCount);
-        urlEntry.decrement(oldTypeCount);
-        userEntry.increment(typeCount);
-        urlEntry.increment(typeCount)
-        .then(() => {
-          db.UrlVote.update({type: type}, {where: {userId: userEntry.id, urlId: urlEntry.id}});
-          res.status(201).json(urlEntry.id);
-        });
+  db.Url.findOne({ where: { id: urlId } }).then(urlEntry => {
+    return db.User
+      .findOne({ where: { username: username } })
+      .then(userEntry => {
+        return db.UrlVote
+          .findOne({ where: { userId: userEntry.id, urlId: urlEntry.id } })
+          .then(voteEntry => {
+            let oldTypeCount = `${voteEntry.type}Count`;
+            let oldType = voteEntry.type;
+            userEntry.decrement(oldTypeCount);
+            urlEntry.decrement(oldTypeCount);
+            userEntry.increment(typeCount);
+            urlEntry.increment(typeCount).then(() => {
+              db.UrlVote.update(
+                { type: type },
+                { where: { userId: userEntry.id, urlId: urlEntry.id } }
+              );
+              res.status(201).json(urlEntry.id);
+            });
+          });
       });
-    });
   });
 });
 
-router.get('/:urlId', (req, res, next) => {
+router.get("/:urlId", (req, res, next) => {
   let urlId = req.params.urlId;
-  db.Url.findOne({where: {id: urlId}})
-  .then(urlEntry => {
-    let rating = utils.calculateRating(urlEntry.upvoteCount, urlEntry.downvoteCount);
-    res.json(rating);
-  })
-  .catch(err => {
-    console.error('error getting URL votes: ', err);
-    res.sendStatus(500);
-  });
+  db.Url
+    .findOne({ where: { id: urlId } })
+    .then(urlEntry => {
+      let rating = utils.calculateRating(
+        urlEntry.upvoteCount,
+        urlEntry.downvoteCount
+      );
+      res.json(rating);
+    })
+    .catch(err => {
+      console.error("error getting URL votes: ", err);
+      res.sendStatus(500);
+    });
 });
 
-router.delete('/', (req, res, next) => {
+router.delete("/", (req, res, next) => {
   let urlId = req.query.urlId;
   let type = req.query.type;
   let username = req.query.username;
   let typeCount = `${type}Count`;
 
-  db.Url.findOne({'where': {'id': urlId}})
-  .then(urlEntry => {
-    return db.User.findOne({'where': {'username': username}})
-    .then(userEntry => {
-      return db.UrlVote.findOne({'where': {'userId': userEntry.id, 'urlId': urlEntry.id}})
-      .then(voteEntry => {
-        userEntry.decrement(typeCount);
-        urlEntry.decrement(typeCount);
-        voteEntry.destroy();
-        res.status(201).json(urlEntry.id);
+  db.Url.findOne({ where: { id: urlId } }).then(urlEntry => {
+    return db.User
+      .findOne({ where: { username: username } })
+      .then(userEntry => {
+        return db.UrlVote
+          .findOne({ where: { userId: userEntry.id, urlId: urlEntry.id } })
+          .then(voteEntry => {
+            userEntry.decrement(typeCount);
+            urlEntry.decrement(typeCount);
+            voteEntry.destroy();
+            res.status(201).json(urlEntry.id);
+          });
       });
-    });
   });
 });
 
