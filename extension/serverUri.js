9948aef02d2362ed4de08ea47a092246be69bcd0
Ciaran Martin Conners <ciaranconners@gmail.com>
2017-08-07 09:41:11 -0400

    changes to extension for deployment

--- extension/serverUri.js
+++ extension/serverUri.js
@@ -1,2 +1,4 @@
 // window.serverUri = 'http://ec2-52-36-33-73.us-west-2.compute.amazonaws.com';
-window.serverUri = 'http://localhost:8080';
+// window.serverUri = 'http://localhost:8080';
+
+window.serverUri = 'http://198.199.71.9:8080';
